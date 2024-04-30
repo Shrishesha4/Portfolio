c90081b30b479e147018b7e447e7dc9de97f92dc
Shrishesha <shrisheshanarmatesshvara@gmail.com>
2024-04-30 12:13:42 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,25 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBrFfOELyuu51wSC1vfGqC4LULE9hhhJ5A",
-  authDomain: "shrishesha4.firebaseapp.com",
-  projectId: "shrishesha4",
-  storageBucket: "shrishesha4.appspot.com",
-  messagingSenderId: "1066466188153",
-  appId: "1:1066466188153:web:b88776360477c867adada8",
-  measurementId: "G-3S6LVYBZRF"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 
 // Typing animation
 var typed = new Typed(".typing", {
@@ -103,3 +84,24 @@ function asideSectionTogglerBtn() {
   aside.classList.toggle("open");
   navTogglerBtn.classList.toggle("open");
 }
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBrFfOELyuu51wSC1vfGqC4LULE9hhhJ5A",
+  authDomain: "shrishesha4.firebaseapp.com",
+  projectId: "shrishesha4",
+  storageBucket: "shrishesha4.appspot.com",
+  messagingSenderId: "1066466188153",
+  appId: "1:1066466188153:web:b88776360477c867adada8",
+  measurementId: "G-3S6LVYBZRF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
